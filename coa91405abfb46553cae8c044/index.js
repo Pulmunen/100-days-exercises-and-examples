d46bb2a7f914ade98d9e97aed7aa64b5f7d84629
Pulmunen <55438344+Pulmunen@users.noreply.github.com>
2021-05-07 00:24:48 +0300

    Delete index.js

--- coa91405abfb46553cae8c044/index.js
+++
@@ -1,9 +0,0 @@
-let hands = ["rock", "paper", "scissor"]
-
-// Create a function that returns a random item from the array
-function rockPaperScissors(){
-let decision = Math.floor(Math.random()*3)
-let outcome = hands[decision]
-console.log(outcome)   
-}
-rockPaperScissors()
