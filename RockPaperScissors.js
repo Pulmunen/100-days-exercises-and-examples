26bb9432ccd51a00dfd0278e895c7f77e45845e6
Pulmunen <55438344+Pulmunen@users.noreply.github.com>
2021-05-07 00:33:27 +0300

    Update RockPaperScissors.js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,6 +1,6 @@
 let hands = ["rock", "paper", "scissor"]
 
-// Create a function that returns a random item from the array
+
 function rockPaperScissors(){
 let decision = Math.floor(Math.random()*3)
 let outcome = hands[decision]
